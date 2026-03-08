b5bdb60787eaae2b1df5fd15eba793bd568d1f8d
willxxy <90741489+willxxy@users.noreply.github.com>
2026-03-08 17:04:25 -0400

    Update and rename fuyu.py to base_elf.py

--- src/elms/llm_encoders/base_elf.py
+++ src/elms/llm_encoders/base_elf.py
@@ -2,9 +2,9 @@ from torch import nn
 import torch
 
 
-class Fuyu(nn.Module):
+class BaseElf(nn.Module):
     def __init__(self, llm: nn.Module, project_layer: nn.Module, only_text: bool = False):
-        super(Fuyu, self).__init__()
+        super(BaseElf, self).__init__()
         self.project_layer = project_layer
         self.llm = llm
         self.only_text = only_text
@@ -40,9 +40,9 @@ class Fuyu(nn.Module):
             B, T, H = llm_embeddings.shape
 
             if projected_embeds.ndim == 2:
-                projected_embeds = projected_embeds.unsqueeze(1)
+                projected_embeds = projected_embeds.unsqueeze(0)
             if signal_id_indices.ndim == 1:
-                signal_id_indices = signal_id_indices.unsqueeze(1)
+                signal_id_indices = signal_id_indices.unsqueeze(0)
 
             assert projected_embeds.shape[:2] == signal_id_indices.shape
             assert projected_embeds.shape[0] == B and projected_embeds.shape[2] == H
