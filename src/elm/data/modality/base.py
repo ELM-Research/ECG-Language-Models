fd704b5fb1e2dc5507fbbefad79c973bb3194440
willxxy <willhan327@outlook.com>
2026-07-27 14:23:09 -0400

    looking better when restricting to signal

--- src/elm/data/modality/base.py
+++ src/elm/data/modality/base.py
@@ -1,18 +1,21 @@
 import random
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Literal
 
 class Base:
     def __init__(self, data,
                  ecg_modality,
                  text_preparer,
                  norm_eps: float = 1e-6,
-                 augmentation: bool = None,
-                 perturbation: str = None):
+                 augmentation: bool = False,
+                 perturbation: Literal["blackout", "gaussian"] = None):
         self.data = data
         self.ecg_modality = ecg_modality
         self.text_preparer = text_preparer
         self.norm_eps = norm_eps
+        self.augmentation = augmentation
+        # no only_text perturbation here, we do that at the LLM level
+        self.perturbation = perturbation
 
     def __len__(self) -> int:
         return len(self.data)
@@ -26,35 +29,37 @@ class Base:
         prepared_ecg_input = self.prepare_ecg(ecg_signal)
 
 
-    def prepare_ecg(self,):
-        pass
+    def prepare_ecg(self, ecg_signal):
+        if self.perturbation == "blackout": ecg_input = self.blackout_ecg(ecg_signal.shape)
+        elif self.perturbation == "gaussian": ecg_input = self.gaussian_ecg(ecg_signal.shape)
 
-    def normalize(self, ecg_signal: np.ndarray) -> Tuple[np.ndarray, Tuple[float, float]]:
+        if self.augmentation: ecg_input = self.augment_ecg(ecg_signal)
+
+        normalized_ecg_input = self.normalize_ecg(ecg_input)
+
+    def normalize_ecg(self, ecg_signal: np.ndarray) -> Tuple[np.ndarray, Tuple[float, float]]:
         min_vals, max_vals = np.min(ecg_signal), np.max(ecg_signal)
         normalized = (ecg_signal - min_vals) / (max_vals - min_vals + self.norm_eps)
         clipped_normalized = np.clip(normalized, 0, 1)
-        return clipped_normalized, (min_vals, max_vals)
+        return clipped_normalized
 
     ### Perturbations
-    def blackout_ecg(self, ecg_shape):
-        c = np.random.choice(np.arange(10))
-        return np.full(ecg_shape, c)
+    def blackout_ecg(self, ecg_shape): return np.full(ecg_shape, np.random.choice(np.arange(10)))
 
-    def gauss_noise_ecg(self, ecg_shape):
-        return np.random.randn(ecg_shape)
+    def gaussian_ecg(self, ecg_shape): return np.random.randn(ecg_shape)
 
     ### Augmentations
-    def augment_ecg(self, signal):
+    def augment_ecg(self, ecg_signal):
         if random.random() < 0.5:
             noise_level = 0.05
-            noise = np.random.normal(0, noise_level * np.std(signal), signal.shape)
-            perturbed_signal = signal + noise
+            noise = np.random.normal(0, noise_level * np.std(ecg_signal), ecg_signal.shape)
+            perturbed_signal = ecg_signal + noise
 
             if random.random() < 0.5:
-                wander_amplitude = 0.07 * np.max(np.abs(signal))
-                wander = wander_amplitude * np.sin(np.linspace(0, random.randint(1, 5) * np.pi, signal.shape[1]))
-                wander = np.tile(wander, (signal.shape[0], 1))
+                wander_amplitude = 0.07 * np.max(np.abs(ecg_signal))
+                wander = wander_amplitude * np.sin(np.linspace(0, random.randint(1, 5) * np.pi, ecg_signal.shape[1]))
+                wander = np.tile(wander, (ecg_signal.shape[0], 1))
                 perturbed_signal += wander
 
             return perturbed_signal
-        return signal
+        return ecg_signal
