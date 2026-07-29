6e076e5c12e54cf0a606027e1998a32511c2b4ab
willxxy <willhan327@outlook.com>
2026-07-29 00:04:54 -0400

    rgb

--- src/elm/data/modality/rgb.py
+++ src/elm/data/modality/rgb.py
@@ -1,2 +1,16 @@
+import random
+import numpy as np
+from PIL import Image
+
 class RGB:
-    pass
+    ### SIGNAL TO IMAGE FUNCTIONS ###
+    def signal_to_image(self, ecg_signal: np.array):
+        image = self.viz.get_plot_as_image(ecg_signal, 250, lead_names = self.lead_names)  # 250 Hz
+        if self.args.augment_rgb and random.random() < 0.6:
+            return self.augment_image(image)
+        return Image.fromarray(image)
+
+    ### IMAGE AUGMENTATION FUNCTIONS ###
+    def augment_image(self, image: np.array):
+        image = Image.fromarray(image)
+        return self.aug(image)
