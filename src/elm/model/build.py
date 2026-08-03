e9828072be42329cd36ab47723937abb47472ba8
willxxy <willhan327@outlook.com>
2026-08-03 02:06:47 -0400

    more progress on model building

--- src/elm/model/build.py
+++ src/elm/model/build.py
@@ -4,17 +4,25 @@ from elm.utils.parallelism import is_main
 class BuildELM:
     def __init__(self, elm_name: str,
                  llm_name: str,
-                 llm_tokenizer,
+                 vocab_size: str,
+                 encoder_name: str,
                  elm_checkpoint: str = None,
                  encoder_checkpoint: str = None,
                  attn_implementation: str = "sdpa",
                  llm_pretrained: bool = True,
-                 peft: str = None
+                 peft: str = None,
+                 gradient_checkpointing: bool = True,
+                 lora_rank: int =  16,
+                 lora_alpha: int = 32,
                  ):
         self.elm_checkpoint = elm_checkpoint
-        self.llm_builder = BuildLLM(llm_name, llm_tokenizer,
-                                    attn_implementation)
-        self.encoder_builder = BuildEncoder()
+        self.llm_builder = BuildLLM(llm_name, vocab_size,
+                                    llm_pretrained, peft,
+                                    attn_implementation,
+                                    gradient_checkpointing,
+                                    lora_rank, lora_alpha)
+        self.encoder_builder = BuildEncoder(encoder_name,
+                                            encoder_checkpoint)
 
     def build_elm(self, ):
         llm_components = self.llm_builder.build_llm()
@@ -35,13 +43,21 @@ from peft import LoraConfig, TaskType, get_peft_model
 from elm.model.llm.hf_llm import HFLLM
 class BuildLLM:
     def __init__(self, llm_name: str,
-                 llm_tokenizer,
+                 vocab_size: str,
                  llm_pretrained: bool = True,
-                 peft: str = None):
+                 peft: str = None,
+                 attn_implementation: str = "sdpa",
+                 gradient_checkpointing: bool = True,
+                 lora_rank: int =  16,
+                 lora_alpha: int = 32,):
         self.llm_name = llm_name
-        self.llm_tokenizer = llm_tokenizer
+        self.vocab_size = vocab_size
         self.llm_pretrained = llm_pretrained
         self.peft = peft
+        self.attn_implementation = attn_implementation
+        self.gradient_checkpointing = gradient_checkpointing
+        self.lora_rank = lora_rank
+        self.lora_alpha = lora_alpha
 
     def build_llm(
         self,
@@ -59,39 +75,28 @@ class BuildLLM:
         if self.llm_pretrained:
             hf_llm = AutoModelForCausalLM.from_pretrained(
                 self.llm_name,
-                attn_implementation=self.args.attention_type,
-            )
+                attn_implementation=self.attn_implementation,
+                )
         else:
             config = AutoConfig.from_pretrained(self.llm_name)
             hf_llm = AutoModelForCausalLM.from_config(config)
-        HF_LLMS[self.args.llm]["model_hidden_size"] = hf_llm.config.hidden_size
-        assert HF_LLMS[self.args.llm]["model_hidden_size"] is not None, print("model_hidden_size")
         hf_llm = self.resize_and_report_embeddings(hf_llm)
-        if self.args.gradient_checkpointing:
+        if self.gradient_checkpointing:
             hf_llm.gradient_checkpointing_enable(gradient_checkpointing_kwargs={"use_reentrant": False})
             hf_llm.config.use_cache = False
-        if self.args.peft:
-            hf_llm = self.build_peft(
-                hf_llm,
-            )
+        if self.peft: hf_llm = self.build_peft(hf_llm,)
         return hf_llm
 
     ### HF LLM FUNCTIONS ###
-    def print_llm_dtype(self, llm):
-        print(
-            f"{self.args.llm} native dtype:", HF_LLMS[self.args.llm]["native_dtype"], f"\n{self.args.llm} actual dtype:", next(llm.parameters()).dtype
-        )
-        assert HF_LLMS[self.args.llm]["native_dtype"] == next(llm.parameters()).dtype, print(f"{self.args.llm} native and actual dtype do not match")
+    def print_llm_dtype(self, llm): print(f"\n{self.llm_name} actual dtype:", next(llm.parameters()).dtype)
 
     def resize_and_report_embeddings(self, hf_llm):
         old_size = hf_llm.get_input_embeddings().weight.shape[0]
-        if is_main():
-            print(f"[{self.args.llm}] embedding size before: {old_size}")
-        hf_llm.resize_token_embeddings(len(self.llm_tokenizer))
+        if is_main(): print(f"Embedding size before: {old_size}")
+        hf_llm.resize_token_embeddings(self.vocab_size)
         new_size = hf_llm.get_input_embeddings().weight.shape[0]
-        if is_main():
-            print(f"[{self.args.llm}] embedding size after: {new_size}")
-        assert new_size == len(self.llm_tokenizer), f"Embedding size {new_size} does not match tokenizer vocab size {len(self.llm_tokenizer)}"
+        if is_main(): print(f"Embedding size after: {new_size}")
+        assert new_size == self.vocab_size, f"Embedding size {new_size} does not match tokenizer vocab size {self.vocab_size}"
         return hf_llm
 
     ### PEFT FUNCTIONS ###
@@ -100,124 +105,44 @@ class BuildLLM:
         llm,
     ):
         lora_config = self.get_lora_configs()
+        lora_config = LoraConfig(
+                    r=self.lora_rank,
+                    lora_alpha=self.lora_alpha,
+                    task_type=TaskType.CAUSAL_LM,
+                )
         llm = get_peft_model(llm, lora_config)
-        if is_main():
-            llm.print_trainable_parameters()
+        if is_main(): llm.print_trainable_parameters()
         return llm
 
-    def get_lora_configs(
-        self,
-    ):
-        lora_config = LoraConfig(
-            r=self.args.lora_rank,
-            lora_alpha=self.args.lora_alpha,
-            task_type=TaskType.CAUSAL_LM,
-        )
-        return lora_config
-
-
 class BuildEncoder:
-    def __init__(self, args: argparse.Namespace):
-        self.args = args
+    def __init__(self, encoder_name: str,
+                 encoder_checkpoint : str = None,
+                 ):
+        self.encoder_name = encoder_name
+        self.encoder_checkpoint = encoder_checkpoint
 
     def build_encoder(self):
-        if self.args.encoder == "merl":
-            encoder_components = self.prepare_merl()
-        elif self.args.encoder == "mlae":
-            encoder_components = self.prepare_mlae()
-        elif self.args.encoder == "mtae":
-            encoder_components = self.prepare_mtae()
-        elif self.args.encoder == "st_mem":
-            encoder_components = self.prepare_st_mem()
-        elif self.args.encoder == "clip-vit-base-patch32":
-            encoder_components = self.prepare_hf_clip()
-        elif self.args.encoder == "siglip2-so400m-patch16-naflex":
-            encoder_components = self.prepare_hf_siglip()
-        elif self.args.encoder == "siglip-ecg":
-            encoder_components = self.prepare_siglip_ecg()
-        elif self.args.encoder == "vit-base-patch16-224-in21k":
-            encoder_components = self.prepare_hf_vit()
-        else:
-            encoder_components = {}
+        if self.encoder_name == "siglep":
+            encoder_components = self.prepare_siglep()
+
         assert encoder_components is not None, print("NN Components is None")
 
-        if self.args.encoder_ckpt:
+        if self.encoder_checkpoint:
             self.load_nn_checkpoint(encoder_components)
 
         return encoder_components
 
-    def prepare_hf_vit(self, ):
-        from transformers import ViTForMaskedImageModeling
-        from elms.vision_encoders.hf_vit.hf_vit import HFVit
-        hf_encoder = ViTForMaskedImageModeling.from_pretrained(VISION_ENCODERS[self.args.encoder]["model"])
-        VISION_ENCODERS[self.args.encoder]["projection_dim"] = hf_encoder.config.hidden_size
-        VISION_ENCODERS[self.args.encoder]["model_hidden_size"] = hf_encoder.config.hidden_size
-        VISION_ENCODERS[self.args.encoder]["num_patches"] = (hf_encoder.config.image_size // hf_encoder.config.patch_size) ** 2
-        assert VISION_ENCODERS[self.args.encoder]["num_patches"] is not None, print("num_patches is None")
-        model = HFVit(hf_encoder, VISION_ENCODERS[self.args.encoder]["output_hidden_states"])
-        return {"encoder": model}
-
-    def prepare_hf_clip(self,):
-        from transformers import AutoModel
-        from elms.vision_encoders.hf_clip.hf_clip import HFClip
-        hf_encoder = AutoModel.from_pretrained(VISION_ENCODERS[self.args.encoder]["model"])
-        VISION_ENCODERS[self.args.encoder]["model_hidden_size"] = hf_encoder.config.projection_dim
-        VISION_ENCODERS[self.args.encoder]["projection_dim"] = hf_encoder.config.projection_dim
-        model = HFClip(hf_encoder, VISION_ENCODERS[self.args.encoder]["output_hidden_states"])
-        return {"encoder": model}
-
-    def prepare_hf_siglip(self,):
-        from transformers import AutoModel
-        from elms.vision_encoders.hf_siglip.hf_siglip import HFSiglip
-        hf_encoder = AutoModel.from_pretrained(VISION_ENCODERS[self.args.encoder]["model"])
-        VISION_ENCODERS[self.args.encoder]["model_hidden_size"] = hf_encoder.config.text_config.hidden_size
-        VISION_ENCODERS[self.args.encoder]["projection_dim"] = hf_encoder.config.text_config.hidden_size
-        model = HFSiglip(hf_encoder, VISION_ENCODERS[self.args.encoder]["output_hidden_states"])
-        return {"encoder": model}
-
-    def prepare_siglip_ecg(self,):
+    def prepare_siglep(self,):
         from transformers import AutoModel, AutoConfig
-        from elms.vision_encoders.siglip_ecg.siglip_ecg import SiglipEcg
-        config = AutoConfig.from_pretrained(VISION_ENCODERS[self.args.encoder]["model"])
-        config.text_config.bos_token_id = None
-        config.text_config.eos_token_id = None
-        hf_encoder = AutoModel.from_pretrained(VISION_ENCODERS[self.args.encoder]["model"], config = config)
+        from elm.model.encoder.siglep.siglep import SigLEP
+        config = AutoConfig.from_pretrained(self.encoder_checkpoint)
+        hf_encoder = AutoModel.from_pretrained(self.encoder_checkpoint, config = config)
         hidden = hf_encoder.config.vision_config.hidden_size
-        VISION_ENCODERS[self.args.encoder]["model_hidden_size"] = hidden
-        VISION_ENCODERS[self.args.encoder]["projection_dim"] = hidden
-        model = SiglipEcg(hf_encoder, segment_len=self.args.segment_len,
+        model = SigLEP(hf_encoder, segment_len=self.args.segment_len,
                           patch_size=25,
                           num_encoder_tokens=self.args.num_encoder_tokens,
                           num_leads=len(self.args.leads))
-        return {"encoder": model}
-
-    def prepare_merl(self,):
-        from elms.ecg_encoders.merl.merl import MerlConfig, Merl
-        cfg = MerlConfig(distributed=self.args.distributed,
-                         num_encoder_tokens=self.args.num_encoder_tokens)
-        model = Merl(cfg)
-        return {"encoder": model}
-
-    def prepare_mlae(self):
-        from elms.ecg_encoders.mlae.mlae import MLAEConfig, MLAE
-        cfg = MLAEConfig(seq_len=self.args.segment_len,
-                         num_encoder_tokens=self.args.num_encoder_tokens) # Each lead is patch, so default
-        model = MLAE(cfg)
-        return {"encoder": model}
-
-    def prepare_mtae(self):
-        from elms.ecg_encoders.mtae.mtae import MTAEConfig, MTAE
-        cfg = MTAEConfig(seq_len=self.args.segment_len, patch_size=self.calculate_patch_size(),
-                         num_encoder_tokens=self.args.num_encoder_tokens)
-        model = MTAE(cfg)
-        return {"encoder": model}
-
-    def prepare_st_mem(self):
-        from elms.ecg_encoders.st_mem.st_mem import ST_MEMConfig, ST_MEM
-        cfg = ST_MEMConfig(seq_len=self.args.segment_len, patch_size=self.calculate_patch_size(),
-                           num_encoder_tokens=self.args.num_encoder_tokens)
-        model = ST_MEM(cfg)
-        return {"encoder": model}
+        return {"encoder": model,}
 
     def load_nn_checkpoint(self, encoder_components):
         ckpt = torch.load(self.args.encoder_ckpt, map_location="cpu", weights_only=False)
@@ -246,22 +171,6 @@ class BuildEncoder:
                     for k in sorted(unused_in_ckpt):
                         print(f"    - {k}")
                 print(f"  Loaded: {len(loaded_keys)}/{len(model_keys)} model layers\n")
-
-    def calculate_patch_size(self):
-        min_patches = 16
-        max_patches = 64
-        factors = [i for i in range(1, self.args.segment_len + 1) if self.args.segment_len % i == 0]
-        patch_candidates = []
-        for patch_size in factors:
-            num_patches = self.args.segment_len // patch_size
-            if min_patches <= num_patches <= max_patches:
-                patch_candidates.append(patch_size)
-        if not patch_candidates:
-            target = int(np.sqrt(self.args.segment_len / 32))
-            patch_size = min(factors, key=lambda x: abs(x - target))
-        else:
-            patch_size = min(patch_candidates, key=lambda x: abs(self.args.segment_len // x - 32))
-        return patch_size
 
 
 def merge_dicts(*parts: Mapping[str, Any], allow_override: Iterable[str] = ()) -> Dict[str, Any]:
