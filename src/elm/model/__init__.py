35dadaa2cfd7552797459eafee67473c42da8d15
willxxy <willhan327@outlook.com>
2026-08-03 23:26:37 -0400

    update

--- src/elm/model/__init__.py
+++ src/elm/model/__init__.py
@@ -1,6 +1,12 @@
 from importlib import import_module
 
+<<<<<<< HEAD
 
 def build_model(config, tokenizer):
     module = import_module(f".{config['model']['name']}", __name__)
     return module.build(config, tokenizer)
+=======
+def build_model(config, tokenizer):
+    module = import_module(f".{config['model']['name']}", __name__)
+    return module.build(config, tokenizer)
+>>>>>>> e739234 (update)
