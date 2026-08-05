8ddca085428ff830d50ca92633fa8631be676fea
willxxy <willhan327@outlook.com>
2026-08-05 01:16:18 -0400

    fix llm name parsing

--- src/elms/build_llm.py
+++ src/elms/build_llm.py
@@ -30,7 +30,7 @@ class BuildLLM:
                 from elms.llms.qwen35.qwen35 import Qwen35
                 model = Qwen35(llm, self.pad_token_id, self.eos_token_id,
                                HF_LLMS[self.args.llm]["output_hidden_states"])
-            elif "qwen3" in self.args.llm:
+            elif "qwen3-" in self.args.llm:
                 from elms.llms.qwen3.qwen3 import Qwen3
                 model = Qwen3(llm, self.pad_token_id, self.eos_token_id,
                               HF_LLMS[self.args.llm]["output_hidden_states"])
