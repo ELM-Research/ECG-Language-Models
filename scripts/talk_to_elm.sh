83b4e4007be998c32fd7a43d223e59f500b25848
Tony <tc3416@columbia.edu>
2026-08-05 20:37:45 -0400

    elm inference

---
+++ scripts/talk_to_elm.sh
@@ -0,0 +1,18 @@
+python3 ./src/main_chat2.py \
+--system_prompt src/dataloaders/system_prompts/system_prompt_original.txt \
+--llm qwen3.5-0.8b \
+--elm mlp_llava \
+--encoder siglip-ecg \
+--elm_ckpt ./src/runs/mlp_llava_qwen3.5-0.8b_siglip-ecg/ecg-qa-ptbxl-250-2500/1/checkpoints/epoch_best.pt \
+--encoder_ckpt ../ecg_encoder/src/runs/pretrain/siglip2-base-patch16-naflex/1/checkpoints/epoch_best.pt \
+--num_encoder_tokens 25
+
+
+#data/ptb_xl/preprocessed_2500/records500_05000_05725_hr_0.npy
+#Sinus rhythm. Normal ECG.
+
+#data/ptb_xl/preprocessed_2500/records500_21000_21555_hr_0.npy
+#Sinus rhythm with left ventricular pattern; moderate voltage criteria suggesting left ventricular hypertrophy; ST/T abnormalities possibly indicating anterior or lateral ischemia or left ventricular overload. Unconfirmed report.
+
+#data/ptb_xl/preprocessed_2500/records500_18000_18812_hr_0.npy
+#Atrial fibrillation/flutter with left-sided QRS/T abnormality; anteroseptal and inferior infarction; possible anterolateral ischemia or left ventricular overload. Unconfirmed findings.
